9c122f2dae92fdaea138337c6892f2f1c9478b39
VasquezDelgado_Jorge <cejots71@gmail.com>
2025-10-26 22:24:39 -0600

    se sube la tarea 2

--- jorge leonardo vasquez delgado/clase 6.py
+++ jorge leonardo vasquez delgado/clase 6.py
@@ -21,5 +21,4 @@ print("________________________________")
 for i in range(1, 11):
     resultado = numero_usuario * i
     print(f'{numero_usuario} x {i} = {resultado}')
-    print("● █▀█▄ Ɑ͞ ̶͞ ̶͞ ̶͞ لں͞!")
 print("________________________________")
